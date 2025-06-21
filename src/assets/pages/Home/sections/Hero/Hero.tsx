2f67517cd465175677059206feb5807e1c09126a
S4mw3i <mrinc.infotek@gmail.com>
2025-06-21 19:36:26 -0300

    fix: Grid import from MUI and Grid size on Hero.tsx, package-lock.json and package.json updated

--- src/assets/pages/Home/sections/Hero/Hero.tsx
+++ src/assets/pages/Home/sections/Hero/Hero.tsx
@@ -1,9 +1,10 @@
-import { Box, Container, Grid, styled, Typography } from "@mui/material"
+import { Box, Container, styled, Typography } from "@mui/material"
 import Avatar from "../../../../../assets/images/avatar.png"
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import StyledButton from "../../../../../components/StyledButton/StyledButton";
 import { AnimatedBackground } from "../../../../../components/AnimatedBackground/AnimatedBackground";
+import Grid from '@mui/material/Grid';
 
 const Hero = () => {
   
@@ -31,7 +32,7 @@ const Hero = () => {
         <StyledHero>
             <Container>
                 <Grid container spacing={2}>
-                    <Grid item xs={12} md={5}>
+                    <Grid size={{ xs:12, md:5 }}>
                         <Box position="relative">
                             <Box position="absolute" width="150%" top={-100} right={0}>
                                     <AnimatedBackground />
@@ -42,13 +43,13 @@ const Hero = () => {
                         </Box>
                         
                     </Grid>
-                    <Grid item xs={12} md={7}>
+                    <Grid size={{ xs:12, md:7 }}>
                         <Typography color="primary.contrastText" variant="h1" textAlign="center" pb={2}>Samuel Muniz
                         </Typography>
                         <Typography color="primary.contrastText" variant="h2" textAlign="center">I'm a Software Developer
                         </Typography>
                         <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
-                            <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                            <Grid size={{ xs:12, md:4 }} display="flex" justifyContent="center">
                                 <StyledButton onClick={()=> console.log("download")}>
                                     <DownloadIcon />
                                     <Typography>
@@ -56,7 +57,7 @@ const Hero = () => {
                                     </Typography>
                                 </StyledButton>
                             </Grid>
-                            <Grid item xs={12} md={4} display="flex" justifyContent="center">
+                            <Grid size={{ xs:12, md:4 }} display="flex" justifyContent="center">
                                 <StyledButton onClick={()=> console.log("contact")}>
                                     <MailOutlineIcon />
                                     <Typography>
