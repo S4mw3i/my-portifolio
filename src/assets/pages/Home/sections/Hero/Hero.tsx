cd331f0a4bee3a7f9ad6004822725c7712d3e930
S4mw3i <mrinc.infotek@gmail.com>
2025-06-21 16:47:29 -0300

    fix: errors in StyledButton

--- src/assets/pages/Home/sections/Hero/Hero.tsx
+++ src/assets/pages/Home/sections/Hero/Hero.tsx
@@ -4,7 +4,6 @@ import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import StyledButton from "../../../../../components/StyledButton/StyledButton";
 import { AnimatedBackground } from "../../../../../components/AnimatedBackground/AnimatedBackground";
-import { Padding } from "@mui/icons-material";
 
 const Hero = () => {
   
@@ -17,15 +16,15 @@ const Hero = () => {
             paddingTop: "100px",
         },
         [theme.breakpoints.up('md')]: { // >= mobile
-            PaddingTop: "0",
+            paddingTop: "0",
         }
-    }))
+    }));
         
     const StyledImg = styled("img")(({theme})=> ({
         width: "75%",
         borderRadius: "50%",
-        border: '1px solid ${theme.palette.primary.main}'
-    }))
+        border: `1px solid ${theme.palette.primary.main}`
+    }));
 
   return (
     <>
@@ -50,7 +49,7 @@ const Hero = () => {
                         </Typography>
                         <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                             <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <StyledButton>
+                                <StyledButton onClick={()=> console.log("download")}>
                                     <DownloadIcon />
                                     <Typography>
                                         Download CV
@@ -58,8 +57,8 @@ const Hero = () => {
                                 </StyledButton>
                             </Grid>
                             <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <StyledButton>
-                                    <MailOutlineIcon></MailOutlineIcon>
+                                <StyledButton onClick={()=> console.log("contact")}>
+                                    <MailOutlineIcon />
                                     <Typography>
                                         Contact me
                                     </Typography>
@@ -71,6 +70,6 @@ const Hero = () => {
             </Container>
         </StyledHero>    
     </>
-  )
-}
-export default Hero
+  );
+};
+export default Hero;
--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -3,9 +3,10 @@ import theme from "../../theme"
 import type { ReactNode } from "react"
 
 interface StyledButtonProps {
-    chidren: ReactNode
+    children: ReactNode
+    onClick: () => void
 }
-const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
+const StyledButton: React.FC<StyledButtonProps> = ({children,onClick}) => {
   
         const StyledButton = styled("button")(()=> ({
             backgroundColor: "transparent",
@@ -26,7 +27,7 @@ const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
     
     return (
         <>
-            <StyledButton>
+            <StyledButton onClick={onClick}>
                 {children}
             </StyledButton>
         </>
